f864b8608a717f192ca5317dc2ebe0523a17bbc9
Brandi Burnell <brandiburnell6@gmail.com>
2024-02-02 09:14:19 -0700

    pass tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_seq = []
+    # if (length == 0):
+    #     return fib_seq
+    for i in range(length):
+        if (i == 0):
+            fib_seq.append(0)
+        elif i == 1:
+            fib_seq.append(1)
+        else:
+            prev_2_num = fib_seq[-2]
+            prev_1_num = fib_seq[-1]
+            new_num = prev_2_num + prev_1_num
+            fib_seq.append(new_num)
+    print(fib_seq)
     pass
+
+print_fibonacci(9)
